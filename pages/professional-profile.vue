248d3bbeb402899569b9674619b92b6a058dc21b
Jewel.Aniciete <jewelaniciete@gmail.com>
2024-05-27 14:06:12 +0800

    ADD: Professional Profile Page

--- pages/professional-profile.vue
+++ pages/professional-profile.vue
@@ -57,10 +57,72 @@ definePageMeta({
                                                     <div class="w-full flex items-start justify-start text-2xl font-semibold">
                                                         <p class="w-full">0 projects</p>
                                                         <div class="flex justify-end w-full items-right">
-                                                            <a href="https://www.youtube.com/watch?v=eRYZVkxHBls"><img src="assets/images/logo/editt.svg" alt="" class="border w-5 h-5" ></a>
+                                                            <a href="https://www.youtube.com/watch?v=eRYZVkxHBls"><img src="assets/images/logo/editt.svg" alt="" class="border p-1 w-8 h-8" ></a>
                                                         </div>
                                                     </div>
                                                 </div>
+                                                <section class="py-16">
+                                                    <div class="container mx-auto">
+                                                        <div class="grid grid-cols-12 md:gap-12 gap-y-8 md:gap-y-0">
+                                                            <div class="col-span-12 lg:col-span-12">
+                                                                <div class="grid grid-cols-12 md:gap-5">
+                                                                    <div class="col-span-12 lg:col-span-4">
+                                                                        <div class="h-72 flex flex-col items-center justify-center bg-gray-400 p-2 transition-all duration-500 border-2 border-dashed hover:bg-gray-400 hover:underline rounded-md dark:bg-transparent dark:shadow-none">
+                                                                            <div>
+                                                                                <div class="w-auto mt-6 text-center h-80">
+                                                                                    <button class="flex flex-col items-center justify-center p-10">
+                                                                                        <img src="assets/images/renovation/add.svg" alt="" class="w-16 h-16">
+                                                                                        <p class="font-semibold text-gray-900 text-3xl">New Project</p>
+                                                                                    </button>
+                                                                                </div>
+                                                                            </div>
+                                                                        </div>
+                                                                    </div>
+                                                                    <div class="col-span-12 lg:col-span-4">
+                                                                        <div class="p-2 transition-all duration-500 bg-white rounded-md shadow-md dark:bg-transparent dark:shadow-none">
+                                                                            <img src="assets/images/blog/img-05.jpg" alt="" class="img-fluid">
+                                                                            <div class="p-5">
+                                                                                <ul class="flex justify-between mb-3 list-inline">
+                                                                                    <li>
+                                                                                        <p class="mb-0 text-gray-500 dark:text-gray-300"><a href="blog-author.html" class="font-semibold text-muted">Kiera Finchr</a> - July 23, 2021</p>
+                                                                                    </li>
+                                                                                    <li>
+                                                                                        <p class="mb-0 text-gray-500 dark:text-gray-300"><i class="mdi mdi-eye"></i> 247</p>
+                                                                                    </li>
+                                                                                </ul>
+                                                                                <a href="blog-details.html" class="primary-link"><h6 class="text-gray-900 transition-all duration-300 text-17 dark:text-white group-data-[theme-color=violet]:hover:text-violet-500 group-data-[theme-color=sky]:hover:text-sky-500 group-data-[theme-color=red]:hover:text-red-500 group-data-[theme-color=green]:hover:text-green-500 group-data-[theme-color=pink]:hover:text-pink-500 group-data-[theme-color=blue]:hover:text-blue-500">How To Increase Trade Show Traffic</h6></a>
+                                                                                <p class="mt-2 text-gray-500 dark:text-gray-300">Intrinsically incubate intuitive opportunities and real-time potentialities Appropriately communicate one-to-one technology.</p>
+                                                                                <div class="mt-4 font-medium group-data-[theme-color=violet]:text-violet-500 group-data-[theme-color=sky]:text-sky-500 group-data-[theme-color=red]:text-red-500 group-data-[theme-color=green]:text-green-500 group-data-[theme-color=pink]:text-pink-500 group-data-[theme-color=blue]:text-blue-500">
+                                                                                    <a href="blog-details.html" class="form-text ">Read More <i class="uil uil-angle-right-b"></i></a>
+                                                                                </div>
+                                                                            </div>
+                                                                        </div>
+                                                                    </div>
+                                                                    <div class="col-span-12 lg:col-span-4">
+                                                                        <div class="p-2 transition-all duration-500 bg-white rounded-md shadow-md dark:bg-transparent dark:shadow-none">
+                                                                            <img src="assets/images/blog/img-06.jpg" alt="" class="img-fluid">
+                                                                            <div class="p-5">
+                                                                                <ul class="flex justify-between mb-3 list-inline">
+                                                                                    <li>
+                                                                                        <p class="mb-0 text-gray-500 dark:text-gray-300"><a href="blog-author.html" class="font-semibold text-muted">Toby Lees</a> - July 11, 2021</p>
+                                                                                    </li>
+                                                                                    <li>
+                                                                                        <p class="mb-0 text-gray-500 dark:text-gray-300"><i class="mdi mdi-eye"></i> 188</p>
+                                                                                    </li>
+                                                                                </ul>
+                                                                                <a href="blog-details.html" class="primary-link"><h6 class="text-gray-900 transition-all duration-300 text-17 dark:text-white group-data-[theme-color=violet]:hover:text-violet-500 group-data-[theme-color=sky]:hover:text-sky-500 group-data-[theme-color=red]:hover:text-red-500 group-data-[theme-color=green]:hover:text-green-500 group-data-[theme-color=pink]:hover:text-pink-500 group-data-[theme-color=blue]:hover:text-blue-500">How apps is changing the IT world</h6></a>
+                                                                                <p class="mt-2 text-gray-500 dark:text-gray-300">Intrinsically incubate intuitive opportunities and real-time potentialities Appropriately communicate one-to-one technology.</p>
+                                                                                <div class="mt-4 font-medium group-data-[theme-color=violet]:text-violet-500 group-data-[theme-color=sky]:text-sky-500 group-data-[theme-color=red]:text-red-500 group-data-[theme-color=green]:text-green-500 group-data-[theme-color=pink]:text-pink-500 group-data-[theme-color=blue]:text-blue-500">
+                                                                                    <a href="blog-details.html" class="form-text ">Read More <i class="uil uil-angle-right-b"></i></a>
+                                                                                </div>
+                                                                            </div>
+                                                                        </div>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </section>
                                             </div>   
                                         </div>
                                     </div>
